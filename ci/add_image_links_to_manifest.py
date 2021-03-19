56fb5692c44da43dd1d9e8cde0c1a014aaaeb3c4
James Weakley <jameswillisweakley@gmail.com>
2021-03-19 15:37:55 +1100

    Update add_image_links_to_manifest.py

--- ci/add_image_links_to_manifest.py
+++ ci/add_image_links_to_manifest.py
@@ -3,9 +3,9 @@ data = None
 with open('target/manifest.json') as f:
     data = json.load(f)
     for node in data['nodes']:
-        print(node)
+        print(f"Replacing description in node: {node}")
         this_node = data['nodes'][node]
-        data['nodes'][node]['description'] = f"[{data['nodes'][node]['description']}] ![{data['nodes'][node]['name']}](images/{data['nodes'][node]['name']}.png)"
+        data['nodes'][node]['description'] = f"{data['nodes'][node]['description']} ![{data['nodes'][node]['name']}](images/{data['nodes'][node]['name']}.png)"
 
 with open('target/manifest.json', 'w') as f:
     json.dump(data, f)
