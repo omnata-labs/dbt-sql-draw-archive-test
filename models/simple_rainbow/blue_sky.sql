c39b3471557f5768876b64cd9e6a44f5059790fc
James Weakley <jameswillisweakley@gmail.com>
2021-03-19 16:33:28 +1100

    Bit more doco

--- models/simple_rainbow/blue_sky.sql
+++ models/simple_rainbow/blue_sky.sql
@@ -5,5 +5,5 @@ select x,y, case
     {{ rainbow(center=[100,100], height=75,stripe_height=5) }}
     else '#87ceeb'
     end as colour
-from {{ ref('blank_canvas') }}
+from {{ ref('blank_canvas_200_200') }}
 where y>=100
--- models/simple_rainbow/grass.sql
+++ models/simple_rainbow/grass.sql
@@ -1,7 +1,4 @@
 {{ config(materialized='new_layer') }}
-/*
-    Add a blue sky
-*/
 
 select x,y,'#228b22' as colour
 from {{ ref('blue_sky') }}
