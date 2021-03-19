b76066d64703e4a2acaf3564793dc1463fd7f549
James Weakley <jameswillisweakley@gmail.com>
2021-03-19 15:20:29 +1100

    Create a sun using a circle

--- models/simple_rainbow/blue_sky.sql
+++ models/simple_rainbow/blue_sky.sql
@@ -3,6 +3,9 @@
     Add a blue sky
 */
 
-select x,y,'#87ceeb' as colour
+select x,y, case
+    when {{ circle_filled([150,150],20) }} then 'yellow'
+    else '#87ceeb'
+    end as colour
 from {{ ref('blank_canvas') }}
 where y>=100
